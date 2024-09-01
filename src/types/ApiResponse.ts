2901c4ff7444fbaa61035705f6daa2eec25dbc56
samandeepbishnoi <samandeepbishnoi@gamil.com>
2024-09-01 14:20:57 +0530

    sing-up route and verify mail

---
+++ src/types/ApiResponse.ts
@@ -0,0 +1,8 @@
+import { Message } from "@/model/User";
+
+export interface ApiResponse {
+  success: boolean;
+  message: string;
+  isAcceptingMessages?: boolean; // here the ? means they are optional
+  messages?: Array<Message>
+};
